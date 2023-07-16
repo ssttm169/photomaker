c5c142629996b66328929c25d1f5a0ea5fb5612f
Tom Tan <tom26@michaels.com>
2023-07-16 23:55:55 +0800

    first commit

---
+++ dist/main.js
@@ -0,0 +1,2 @@
+#!/usr/bin/env node
+export{v as vuepressPhotomaker}from"./vuepress-de19a72c.js";import"./vendor-5fff49e8.js";import"glob";import"fs";import"sharp";import"path";
---
+++ dist/types/config.d.ts
@@ -0,0 +1,11 @@
+export const PHOTO_CONFIG: Readonly<{
+    appName: "photomaker";
+    version: "1.0.0";
+    storePath: "/photomaker";
+    acceptExt: string[];
+    ext: string;
+    webp: false;
+    watermark: false;
+    quality: 80;
+}>;
+export const DEFAULT_RESIZE_FIX: string[];
